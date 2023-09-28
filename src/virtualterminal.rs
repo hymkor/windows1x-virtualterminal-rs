87ac5b4a26f6be5f1d83dbf28a6b0ad293b64187
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-09-28 13:07:25 +0900

    new module: virtualterminal

---
+++ src/virtualterminal.rs
@@ -0,0 +1,64 @@
+use windows::Win32::System::Console::{
+    CONSOLE_MODE,
+    GetStdHandle,
+    STD_OUTPUT_HANDLE,
+    GetConsoleMode,
+    SetConsoleMode,
+    // STD_HANDLE,
+};
+
+use windows::Win32::Foundation::HANDLE;
+
+const ENABLE_VIRTUAL_TERMINAL_PROCESSING:u32 = 0x4;
+
+struct ConsoleHandle {
+    handle: HANDLE
+}
+
+fn new_console_handle() -> windows::core::Result<ConsoleHandle> {
+    unsafe{
+        let this = ConsoleHandle{
+            handle: GetStdHandle( STD_OUTPUT_HANDLE )?
+        };
+        return Ok(this);
+    }
+}
+
+impl ConsoleHandle {
+    fn get_mode(&self) -> windows::core::Result<CONSOLE_MODE> {
+        unsafe{
+            let mut console_mode = CONSOLE_MODE(0);
+
+            match GetConsoleMode( self.handle  , &mut console_mode ).ok() {
+                Ok(_) => return Ok(console_mode),
+                Err(err) => return Err(err),
+            }
+        }
+    }
+
+    fn set_mode(&self, mode: CONSOLE_MODE) -> windows::core::Result<()> {
+        unsafe{
+            SetConsoleMode( self.handle  , mode );
+            return Ok(());
+        }
+    }
+}
+
+pub struct RewindMode {
+    handle: ConsoleHandle,
+    mode: CONSOLE_MODE,
+}
+
+impl Drop for RewindMode {
+    fn drop(&mut self){
+        let _  = self.handle.set_mode(self.mode);
+    }
+}
+
+pub fn enable() -> windows::core::Result<RewindMode> {
+    let stdout = new_console_handle()?;
+    let mode = stdout.get_mode()?;
+    let _ = stdout.set_mode(
+        CONSOLE_MODE( mode.0 | ENABLE_VIRTUAL_TERMINAL_PROCESSING));
+    return Ok(RewindMode{ handle:stdout , mode:mode })
+}
