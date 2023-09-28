9c049f49e535bb26a3d821e48ff1da7f2483e5a5
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-09-28 21:28:03 +0900

    Change output from executable to lib and make example

---
+++ examples/examples.rs
@@ -0,0 +1,6 @@
+fn main(){
+    if let Ok(_) = virtualterminal::enable_stdout() {
+        println!("\x1B[36msuccess\x1B[0m");
+    }
+    println!("\x1B[36m(AFTER)\x1B[0m");
+}
--- src/main.rs
+++
@@ -1,8 +0,0 @@
-mod virtualterminal;
-
-fn main() {
-    if let Ok(_) = virtualterminal::enable_stdout() {
-        println!("\x1B[36msuccess\x1B[0m");
-    }
-    println!("\x1B[36m(AFTER)\x1B[0m");
-}
