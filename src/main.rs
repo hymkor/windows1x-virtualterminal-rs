1f8a7a812ba8a821f3e13efac86afa275364e9ca
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-09-28 20:32:35 +0900

    Simplify main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
 mod virtualterminal;
 
 fn main() {
-    match virtualterminal::enable_stdout() {
-        Ok(_) => {
-            println!("\x1B[36msuccess\x1B[0m");
-        }
-        Err(err) => {
-            println!("error: {:?}",err);
-        }
+    if let Ok(_) = virtualterminal::enable_stdout() {
+        println!("\x1B[36msuccess\x1B[0m");
     }
     println!("\x1B[36m(AFTER)\x1B[0m");
 }
