69bb357cb597ced59fcac0fc223ebc5e65daf587
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-09-29 10:10:35 +0900

    examples: add comments

--- examples/examples.rs
+++ examples/examples.rs
@@ -1,6 +1,8 @@
-fn main(){
+fn main() {
     if let Ok(_) = virtualterminal::enable_stdout() {
-        println!("\x1B[36msuccess\x1B[0m");
+        println!("\x1B[36m(enabled)\x1B[0m");
     }
-    println!("\x1B[36m(AFTER)\x1B[0m");
+    // The return value of enable_stdout is dropped,
+    // The virtual terminal processing is disabled.
+    println!("\x1B[36m(disabled)\x1B[0m");
 }
--- examples/getkey.rs
+++ examples/getkey.rs
@@ -2,8 +2,12 @@ fn main() {
     if let Ok(_) = virtualterminal::enable_stdin() {
         if let Ok(_) = virtualterminal::make_raw() {
             if let Ok(key) = virtualterminal::getkey() {
-                println!("{}", key.replace("\x1B","ESC") );
+                println!("{}", key.replace("\x1B", "ESC"));
             }
         }
+        // When the return value of make_raw() is droppped,
+        // the mode is changed to cooked mode.
     }
+    // When the return value of enable_stdin() is droppped,
+    // virtual_terminal_input is disabled.
 }
