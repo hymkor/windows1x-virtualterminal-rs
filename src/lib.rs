39873073aa58910f46677b896439b7c54b1be8b2
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-10-04 17:50:25 +0900

    Implement GetKeyError instead of Box<dyn std::error:Error>

--- src/lib.rs
+++ src/lib.rs
@@ -104,18 +104,23 @@ pub fn make_raw() -> Result<OldState> {
     return change(STD_INPUT_HANDLE, RAW_MODE, 0);
 }
 
-pub fn getkey() -> std::result::Result<String, Box<dyn std::error::Error>> {
+pub enum GetKeyError {
+    Io(std::io::Error),
+    FromUtf8(std::string::FromUtf8Error),
+}
+
+pub fn getkey() -> std::result::Result<String, GetKeyError> {
     let mut buffer: Vec<u8> = vec![0; 256];
     let mut stdin = std::io::stdin();
 
     let n = match stdin.read(&mut buffer) {
         Ok(n) => n,
-        Err(err) => return Err(Box::new(err)),
+        Err(err) => return Err(GetKeyError::Io(err)),
     };
     buffer.truncate(n);
     match String::from_utf8(buffer) {
         Ok(s) => return Ok(s),
-        Err(err) => return Err(Box::new(err)),
+        Err(err) => return Err(GetKeyError::FromUtf8(err)),
     }
 }
 
