afcecaa6eba47a2aca448c7623aeb544dd0b7bb4
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-10-04 19:07:51 +0900

    Rename getkey::GetKeyError to getkey::getkey::Error

--- src/getkey.rs
+++ src/getkey.rs
@@ -1,9 +1,9 @@
-pub enum GetKeyError {
+pub enum Error {
     Io(std::io::Error),
     FromUtf8(std::string::FromUtf8Error),
 }
 
-pub fn getkey() -> std::result::Result<String, GetKeyError> {
+pub fn getkey() -> std::result::Result<String, Error> {
     use std::io::Read;
 
     let mut buffer: Vec<u8> = vec![0; 256];
@@ -11,11 +11,11 @@ pub fn getkey() -> std::result::Result<String, GetKeyError> {
 
     let n = match stdin.read(&mut buffer) {
         Ok(n) => n,
-        Err(err) => return Err(GetKeyError::Io(err)),
+        Err(err) => return Err(Error::Io(err)),
     };
     buffer.truncate(n);
     match String::from_utf8(buffer) {
         Ok(s) => return Ok(s),
-        Err(err) => return Err(GetKeyError::FromUtf8(err)),
+        Err(err) => return Err(Error::FromUtf8(err)),
     }
 }
